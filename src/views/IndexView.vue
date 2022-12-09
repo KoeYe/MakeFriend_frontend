4d18826c956279a7448efce80f303de9347fee70
prosperousye <2826232264@qq.com>
2022-12-10 04:31:09 +0800

    pref:优化了亿点点细节

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,21 +1,42 @@
 <script lang="ts" setup>
-import { ref, watch, reactive } from "vue"
+import { ref, watch, reactive,onMounted } from "vue"
 
 import Aside from "../components/AsideComponent/AsideComponent.vue"
 import Session from "../components/SessionComponent/SessionComponent.vue"
 import axios from "axios";
 import { ElMessage } from "element-plus";
 import { useRouter } from "vue-router";
+let collapsed = ref(false)
 const username = sessionStorage.getItem("username");
+let screen_width = ref(document.body.clientWidth)
+let sider_width = ref(document.body.clientWidth*0.23)
+onMounted(
+  window.onresize = () => {
+    screen_width.value = document.body.clientWidth
+    sider_width.value = document.body.clientWidth*0.23
+  }
+)
+watch(screen_width, (newVal, oldVal) => {
+    if (newVal < 1200) {
+      //console.log("newVal", newVal);
+      collapsed.value = true;
+    } else {
+      collapsed.value = false;
+    }
+  }
+)
+const col = () => {
+  collapsed.value = !collapsed.value
+}
 </script>
 
 <template>
   <a-layout class="base-container">
   <!--侧边栏-->
-    <a-layout-sider :resize-directions="['right']" :span="4">
+    <a-layout-sider :collapsed="collapsed" collapsible @collapse="col" :width=sider_width :collapsed-width=0>
       <Aside />
     </a-layout-sider>
-    <a-layout :span="20">
+    <a-layout>
       <router-view />
     </a-layout>
   </a-layout>
@@ -32,7 +53,6 @@ const username = sessionStorage.getItem("username");
 
 .arco-layout-sider{
   max-width: 25%;
-  min-width: 20%;
   padding-left: 10px;
   padding-right: 10px;
   background-color: white;
