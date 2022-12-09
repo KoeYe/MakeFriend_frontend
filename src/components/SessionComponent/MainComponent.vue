59162be82ebb808575cb9390b0e4802efb8449f3
prosperousye <2826232264@qq.com>
2022-12-10 00:41:08 +0800

    feat:添加撤回功能

--- src/components/SessionComponent/MainComponent.vue
+++ src/components/SessionComponent/MainComponent.vue
@@ -4,8 +4,24 @@
         <div ref="innerRef">
         <div v-for="mess of message">
             <div v-if="(mess.user_id==user_id)">
-                <el-row justify="end">
-                    <el-card body-style="padding-top:5px; padding-bottom:5px" style="hover{box-shadow: 2px 2px 2px 2px;}; border-radius: 20px; margin:8px; text-align: right; width:fit-content;background-color: rgb(239, 253, 222);" shadow="hover">
+                <el-row  justify="end">
+                    <a-space align='end'>
+                    <div v-show="visibile(mess.id)==1"
+                        @mouseenter="mouseenter(mess.id)"
+                        @mouseleave="mouseleave(mess.id)">
+                    <a-popconfirm content="Are you sure you want to delete the message?" position="left"
+                                    ok-text="Yes" cancel-text="No" @ok="deleteMessage(mess.id)"
+                                    >
+                        <el-button :icon="Delete" circle class="del-btn"/>
+                    </a-popconfirm>
+                    </div>
+                    <el-card
+                        @mouseenter="mouseenter(mess.id)"
+                        @mouseleave="mouseleave(mess.id)"
+                        body-style="padding-top:5px; padding-bottom:5px"
+                        style="hover{box-shadow: 2px 2px 2px 2px;}; border-radius: 20px; margin:8px; text-align: right; width:fit-content;background-color: rgb(239, 253, 222);"
+                        shadow="hover"
+                    >
                         <div v-if="(mess.type=='text')">
                             <el-row><h4>{{mess.content}}</h4></el-row>
                         </div>
@@ -18,7 +34,8 @@
                         </div>
                         <el-row justify="end" style="font-size:xx-small;margin:0px;color:green">{{(mess.hour<10?'0'+mess.hour:mess.hour)}}:{{(mess.minute<10?'0'+mess.minute:mess.minute)}}</el-row>
                     </el-card>
-                </el-row>
+                </a-space>
+            </el-row>
             </div>
             <div v-if="(mess.user_id!=user_id)">
                 <el-row justify="start">
@@ -43,8 +60,16 @@
 </div>
 </template>
 <script lang="ts" setup>
+import {
+  Check,
+  Delete,
+  Edit,
+  Message,
+  Search,
+  Star,
+} from '@element-plus/icons-vue'
 import axios from "axios";
-import { messageType,ElScrollbar } from "element-plus";
+import { messageType,ElScrollbar, ElMessage } from "element-plus";
 import { ref, watch, onMounted } from "vue"
 import { useRoute } from "vue-router"
 const innerRef = ref<HTMLDivElement>()
@@ -53,6 +78,7 @@ const inputSlider = () => {
     //console.log("setting scroll")
     scrollbarRef.value?.setScrollTop(innerRef.value!.clientHeight-380)
 }
+
 let props = defineProps(["session_id"])
 let message:any = ref([])
 const user_id = sessionStorage.getItem('id')
@@ -71,4 +97,39 @@ const getMessage = () => {
 }
 getMessage()
 setTimeout(()=>{inputSlider()},200)
+let visibile_id = ref()
+const mouseenter = (id:string) => {
+    visibile_id.value = id
+}
+const mouseleave = (id:string) => {
+    setTimeout(()=>{visibile_id.value = ""},3000)
+}
+const visibile = (id:string) => {
+    if(visibile_id.value === id){
+        console.log(visibile_id.value)
+        return 1
+    }else{
+        return 0
+    }
+}
+const deleteMessage = (id:any) => {
+    axios.
+    delete("/api/session/message?message_id="+id)
+    .then((res)=>{
+        ElMessage.success(res.data)
+        //getMessage()
+    })
+}
 </script>
+
+<style scoped>
+.del-btn{
+    background-color: rgba(0, 0, 0, 0.2);
+    border-width: 0;
+    position:relative;
+    bottom: 5px;
+}
+.del-btn:hover{
+    color: black;
+}
+</style>
